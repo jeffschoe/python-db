b0def6f103689e3ffec25b5250b2b67cf975343e
jeffschoe <ratacame@protonmail.com>
2026-08-27 22:08:13 -0400

    S1L9: create GUI

---
+++ mybooks.py
@@ -0,0 +1,29 @@
+from tkinter import (
+    Tk, 
+    Button, 
+    Label, 
+    Scrollbar, 
+    Listbox,
+    StringVar,
+    Entry,
+    W,
+    E,
+    N,
+    S,
+    END
+)
+from tkinter import ttk
+from tkinter import messagebox
+
+
+def main():
+    root = Tk()
+    root.mainloop()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
