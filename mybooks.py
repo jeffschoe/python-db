2fae105dfbf85b2ce6201d0af97d391925516cfd
jeffschoe <ratacame@protonmail.com>
2026-08-27 22:40:26 -0400

    S1L11, more entry boxes

--- mybooks.py
+++ mybooks.py
@@ -24,6 +24,23 @@ def main():
     root.geometry("1700x1000")
     root.resizable(width=False, height=False)
 
+    title_label = ttk.Label(root, text="Title", background="light green", font=("TkDefaultFont", 16))
+    title_label.grid(row=0, column=0, sticky=W)
+    title_text = StringVar()
+    title_entry = ttk.Entry(root, width=24, textvariable=title_text)
+    title_entry.grid(row=0, column=1, sticky=W)
+
+    author_label = ttk.Label(root, text="Author", background="light green", font=("TkDefaultFont", 16))
+    author_label.grid(row=0, column=2, sticky=W)
+    author_text = StringVar()
+    author_entry = ttk.Entry(root, width=24, textvariable=author_text)
+    author_entry.grid(row=0, column=3, sticky=W)
+
+    isbn_label = ttk.Label(root, text="ISBN", background="light green", font=("TkDefaultFont", 16))
+    isbn_label.grid(row=0, column=4, sticky=W)
+    isbn_text = StringVar()
+    isbn_entry = ttk.Entry(root, width=24, textvariable=isbn_text)
+    isbn_entry.grid(row=0, column=5, sticky=W)
 
     root.mainloop()
 
